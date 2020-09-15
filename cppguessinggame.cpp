91d46c1d4721c001603aca25f2118e38ef6a97bb
jlfudeveloper <69946005+jlfudeveloper@users.noreply.github.com>
2020-09-15 08:45:06 -0700

    added comments

--- cppguessinggame.cpp
+++ cppguessinggame.cpp
@@ -8,22 +8,21 @@ int main(){
     srand (time(NULL)); //initialize the random seed
     int randnum = rand()%101; //create the random variable
     int guesses = 0; //counts how many guesses the reader used
-    while(true){
+    while(true){ //while the user has not guessed the number
       cout << "type in a number between 0 and 100: " << endl;
       int guess;
-      cin >> guess;
-      if(guess == randnum){
-	guesses += 1;
-	cout << "you got it! play again? (y/n) " << endl;
+      cin >> guess; //prompts the user for a guess at the number
+      guesses += 1; //update amount of guesses
+      if(guess == randnum){  //if the user got it correct, tell them and ask them if they want to play again
+	cout << "you got it in " << guesses << " guesses! play again? (y/n) " << endl;
 	cin >> playagain;
 	break;
       }
+      //otherwise, tell them if they are higher or lower
       else if(guess > randnum){
-	guesses += 1;
 	cout << "your number is too high! try again! " << endl;
       }
       else{
-	guesses += 1;
 	cout << "your number is too low! try again! " << endl;
       }
     }
